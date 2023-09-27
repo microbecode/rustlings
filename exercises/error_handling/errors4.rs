594e846142cf7593c6062ae77798040f3ea4020f
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-09-27 16:26:59 +0100

    moar

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -17,6 +15,8 @@ enum CreationError {
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm... Why is this always returning an Ok value?
+        if value == 0 { return Err(CreationError::Zero); }
+        if value < 0 { return Err(CreationError::Negative); }
         Ok(PositiveNonzeroInteger(value as u64))
     }
 }
@@ -30,3 +30,4 @@ fn test_creation() {
     );
     assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
 }
+
